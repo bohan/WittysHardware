ea51538bc6639c5a603962f8687160521544daf0
Joel Cofie <jtcofie@gmail.com>
2011-08-08 13:57:02 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WittysHardware::Application.config.secret_token = '1ba8186d194f733b6a7af8a0f7108760ff7c260471c6a6936324c7d13ebfb4dd13ca1242fe26ffe0bbf95e953c5831b4c8dc6d0f0a57944483fcdf17038367c3'
